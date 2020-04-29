9f2b208b2430025440544a8ec930f5ac20a160dc
Yuichiro Smith <contact@yu-smith.com>
2020-04-29 19:02:30 +0900

    add ogp tags and image

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-center">
-        {{ title }}
-        {{ header }}
-      </div>
-    </v-flex>
+    <v-card class="mx-auto" min-width="400">
+      <v-img :aspect-ratio="120 / 63" :src="image" class="white--text align-end">
+        <v-card-title>{{ title }}</v-card-title>
+      </v-img>
+      <v-card-text class="text-primary">
+        <div>{{ description }}</div>
+      </v-card-text>
+    </v-card>
   </v-layout>
 </template>
 
@@ -17,30 +19,37 @@ import { Route } from 'vue-router';
 interface Layout {
   title: string;
   header: string;
+  image: string;
+  description: string;
 }
 
 const defaultLayout: Layout = {
   title: 'Examply.js',
   header: 'Sample Header',
+  description: 'Sample Header',
+  image: 'https://dummyimage.com/1200x630?text=Examply.js',
 };
 
 const getLayout: (route: Route) => Promise<Layout> = (route: Route) =>
   new Promise(resolve =>
-    resolve(
-      Object.assign({}, defaultLayout, {
-        title: route.fullPath.split('/')[1],
-        header: route.fullPath.split('/').reduce((_, value) => value),
-      })
-    )
+    resolve({
+      title: route.fullPath.split('/')[1] || defaultLayout.title,
+      header: route.fullPath.split('/').reduce((prev, value, idx) => (idx > 2 ? prev : value)) || defaultLayout.header,
+      description: route.fullPath.split('/').join(' ') || defaultLayout.description,
+      image: defaultLayout.image,
+    })
   );
 
 @Component({
   async asyncData(ctx) {
     const { BASE_URL } = ctx.env;
     const layout = await getLayout(ctx.route);
+    const image = `https://dummyimage.com/1200x630/345678?text=${layout.header}%20-%20${layout.title}`;
+
     return {
       baseUrl: BASE_URL,
       ...layout,
+      image,
     };
   },
   head(): MetaInfo {
@@ -48,16 +57,21 @@ const getLayout: (route: Route) => Promise<Layout> = (route: Route) =>
     return {
       title: this.title,
       meta: [
+        { hid: 'og:type', property: 'og:type', content: 'website' },
         { hid: 'og:site_name', property: 'og:site_name', content: 'Examply.js' },
-        { hid: 'og:title', property: 'og:title', content: this.title },
+        { hid: 'og:title', property: 'og:title', content: `${this.header} - ${this.title}` },
+        { hid: 'og:description', property: 'og:description', content: this.description },
         { hid: 'og:url', property: 'og:url', content: `${host}${this.$route.fullPath}` },
+        { hid: 'og:image', property: 'og:image', content: this.image },
       ],
     };
   },
 })
-export default class Default extends Vue {
+export default class Default extends Vue implements Layout {
   title!: string;
   header!: string;
   baseUrl!: string;
+  image!: string;
+  description!: string;
 }
 </script>
